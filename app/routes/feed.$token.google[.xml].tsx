c46deb4cd97dfc0ff28806e0ee6f3bacf80930ea
Pavel13579 <pasha.budapest@gmail.com>
2026-07-20 16:22:58 +0200

    Added xml response by token

--- app/routes/feed.$token.google[.xml].tsx
+++ app/routes/feed.$token.google[.xml].tsx
@@ -1,12 +1,16 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
+import db from "../db.server";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const token = params.token;
 
-  return new Response(`token=${token}`, {
-    status: 200,
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8",
-    },
+  if (!token) {
+    return new Response("Token is required", { status: 400 });
+  }
+
+  const feed = await db.feed.findUnique({
+    where: { token },
   });
+
+  return new Response(feed?.content || "");
 }
